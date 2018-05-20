3d7f696cef37e38586b5e40aee07184902a6d00c
Kacia Hosmer <kacia@coconutrobot.com>
2018-05-20 23:00:30 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,13 @@
+var meals = {};
+
+var meals = new Object();
+
+var meals = { breakfast: "oatmeal" }; 
+
+var meals = new Object({ breafast: 'oatmeal' })
+
+var meals = { breakfast: 'oatmeal', 
+              lunch: 'burrito', 
+              dinner: 'steak'
+}
+
