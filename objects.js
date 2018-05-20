1d8a935c0443432811b6539fbc31bf99c1dca7f9
Kacia Hosmer <kacia@coconutrobot.com>
2018-05-20 23:13:51 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,13 +1,21 @@
-var meals = {};
+// var meals = {};
 
-var meals = new Object();
+// var meals = new Object();
 
-var meals = { breakfast: "oatmeal" }; 
+// var meals = { breakfast: "oatmeal" }; 
 
-var meals = new Object({ breafast: 'oatmeal' })
+// var meals = new Object({ breafast: 'oatmeal' })
 
-var meals = { breakfast: 'oatmeal', 
+// var meals = { breakfast: 'oatmeal', 
               lunch: 'burrito', 
               dinner: 'steak'
 }
 
+meals.snack = 'yogurt';
+meals['second breakfast'] = 'bagel';
+var sweetMeal = 'dessert'; 
+meals [sweetMeal] = 'cake';
+
+meals.breakfast = 'cereal';  //
+
+function destructivelyUpdateObjectWithKeyAndValue
