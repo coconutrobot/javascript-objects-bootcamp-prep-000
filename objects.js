03afa9c4fbed48a48cc7de3cfd6cb16c098b492f
Kacia Hosmer <kacia@coconutrobot.com>
2018-05-20 23:26:29 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,9 +1,1 @@
-function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) { 
-  
-    obj[key] = value;
-    return obj ;
-}
-
-const recipe = { eggs:3 }
-
-destructivelyUpdateObjectWithKeyAndValue(recipe, 'flour', '3 eggs')
+var playlist = 
